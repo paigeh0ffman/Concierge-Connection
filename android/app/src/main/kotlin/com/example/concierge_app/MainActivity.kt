f9ada378f9b8c530747ebc887cbe72f21dd2cb9c
Paige Hoffman <paige.hoffman@att.net>
2026-04-18 11:49:05 -0500

    Initial commit with barebones app structure

---
+++ android/app/src/main/kotlin/com/example/concierge_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.concierge_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/patient/patient_messenger.dart
@@ -0,0 +1,6 @@
+/* Patient-side messenger view
+
+Authors: Paige Hoffman
+
+Citations: flutter.dev
+ */
